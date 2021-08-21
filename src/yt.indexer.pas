7744745aec5f47a1d0ffe61bebf1dfd862063168
mr-highball <35942565+mr-highball@users.noreply.github.com>
2021-08-21 17:27:12 -0400

    checkin

--- src/yt.indexer.pas
+++ src/yt.indexer.pas
@@ -8,13 +8,17 @@ uses
   Classes,
   SysUtils,
   fphttpwebclient,
+  fpoauth2,
+  fpwebclient,
   jsonparser,
   googlebase,
   googleservice,
   googleclient,
   googleyoutube,
   opensslsockets,
-  ezjson;
+  ezjson,
+  ezthreads,
+  ezthreads.pool;
 
 type
 
@@ -24,20 +28,31 @@ type
   TYTIndexer = class(TComponent)
   strict private
     FDaily: Integer;
+    FKeys: TStringArray;
     FSpent: Integer;
     FDBName: String;
     FQuotaCost: Integer;
-    FClient : TGoogleClient;
-    FAPI : TYoutubeAPI;
+    FKey : String;
+    FError : String;
+    FPool : IEZThreadPool;
+    FStopRequest : Boolean;
+    function GetError: String;
     function GetKey: String;
     function GetRemaining: Integer;
+    function GetRunning: Boolean;
     procedure SetKey(AValue: String);
     procedure SetRemaining(AValue: Integer);
+    procedure Initialize(const AThread : IEZThread);
+    procedure FetchResults(const AThread : IEZThread);
   strict protected
   public
     procedure Run;
+    procedure Stop;
 
-    constructor Create; virtual;
+    property Running : Boolean read GetRunning;
+    property Error : String read GetError;
+
+    constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
   published
     [JsonProperty('db_file_name')]
@@ -54,9 +69,14 @@ type
 
     [JsonProperty('api_key')]
     property APIKey : String read GetKey write SetKey;
+
+    [JsonProperty('keywords')]
+    property KeyWords : TStringArray read FKeys write FKeys;
   end;
 
 implementation
+uses
+  DateUtils;
 
 { TYTIndexer }
 
@@ -68,14 +88,24 @@ begin
     Result := 0;
 end;
 
+function TYTIndexer.GetRunning: Boolean;
+begin
+  Result := False; //todo...
+end;
+
 function TYTIndexer.GetKey: String;
 begin
-  Result := FClient.AuthHandler.Config.DeveloperKey;
+  Result := FKey;
+end;
+
+function TYTIndexer.GetError: String;
+begin
+  Exit(FError);
 end;
 
 procedure TYTIndexer.SetKey(AValue: String);
 begin
-  FClient.AuthHandler.Config.DeveloperKey := AValue;
+  FKey := AValue;
 end;
 
 procedure TYTIndexer.SetRemaining(AValue: Integer);
@@ -86,49 +116,111 @@ begin
     FSpent := 0;
 end;
 
-procedure TYTIndexer.Run;
+procedure TYTIndexer.Initialize(const AThread: IEZThread);
 var
-  LRes: TSearchResource;
-  LList: TSearchListResponse;
-  LEntry: TSearchResult;
-  LOptions : TSearchListOptions;
+  LNextQueue, LNow: TDateTime;
 begin
-  LRes := FAPI.CreateSearchResource;
+  LNextQueue := Default(TDateTime);
   try
-    try
-      FClient.AuthHandler.RefreshIDToken;
-      LOptions := Default(TSearchListOptions);
-      LOptions.eventType := 'video';
-      LOptions.q := 'doggo';
+    //we'll process until the caller has requested us to stop
+    while not FStopRequest do
+    begin
+      LNow := LocalTimeToUniversal(Now);
 
-      LList := LRes.List(LOptions); //todo...this is requiring auth... but should it with dev key?
+      //see if we need to queue up the index calls
+      if MilliSecondSpan(LNow, LNextQueue) < 0 then
+        begin
+        //determine how many calls we can make for the day
+        //...
 
-      for LEntry in LList.Items do
-        WriteLn(LEntry.SaveToJSON.AsJSON); //todo - change this
-    finally
-      LRes.Free;
+        //now determine how much time we have left in the day by UTC time
+        //...
+
+        //shuffle the keyword list to produce a random smattering of searches
+        //...
+
+        //produce enough calls to the pool to handle the work remaining for the day
+        //...
+          //divide the work by keywords
+          //...
+
+        //set the next 'queue date' to tomorrow
+        //...
+      end
+      else
+        Sleep(3000);
     end;
   except on E : Exception do
-    WriteLn(E.Message); //todo - change this
+    FError := E.Message;
   end;
 end;
 
-constructor TYTIndexer.Create;
+procedure TYTIndexer.FetchResults(const AThread: IEZThread);
+var
+  LList: TSearchListResponse;
+  LClient : TFPHTTPWebClient;
+  LResp: TWebClientResponse;
+  LReq: TWebClientRequest;
 begin
+  LList := TSearchListResponse.Create();
+  LClient := TFPHTTPWebClient.Create(nil);
+  LReq := LClient.CreateRequest;
+  try
+    try
+      //pull the keyword we're searching and time we have to wait until
+      //...
+
+      //wait the elotted time (if any)
+      //Sleep(LWait);
+
+      //attempt to query youtube
+      //...
+      LResp := LClient.ExecuteRequest(
+        'GET',
+        'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=AQUERY&order=date&key=API_KEY',
+         LReq
+      );
+
+      //parse the response to a search list
+      //...
+
+      //save to the db
+      //...
+    finally
+      LList.Free;
+      LClient.Free;
+    end;
+  except on E : Exception do
+    FError := E.Message;
+  end;
+end;
+
+procedure TYTIndexer.Run;
+begin
+  try
+    FStopRequest := False;
+    FPool.Queue(Initialize, nil, nil);
+    FPool.Start;
+  except on E : Exception do
+    FError := E.Message;
+  end;
+end;
+
+procedure TYTIndexer.Stop;
+begin
+  FStopRequest := True;
+  FPool.Stop;
+end;
+
+constructor TYTIndexer.Create(AOwner: TComponent);
+begin
+  inherited;
   FDaily := 10000;
   FQuotaCost := 100;
   FSpent := 0;
   FDBName := 'yt_indexer.db';
-  FClient := TGoogleClient.Create(Self);
-  FClient.WebClient:=TFPHTTPWebClient.Create(Self);
-  FClient.WebClient.LogFile:='yt-indexer.log'; //todo - change this from being static
-  FClient.Config.AuthMethod := amDeveloperKey;
-  FClient.AuthHandler := TGoogleOAuth2Handler.Create(Self);
-  FClient.AuthHandler.Config.DeveloperKey := '';
-  FClient.WebClient.RequestSigner := FClient.AuthHandler;
-  FClient.AuthHandler.WebClient := FClient.WebClient;
-  FAPI := TYoutubeAPI.Create(Self);
-  FAPI.GoogleClient := FClient;
+  FError := '';
+  FPool := NewEZThreadPool();
 end;
 
 destructor TYTIndexer.Destroy;
--- yt_indexer.lpr
+++ yt_indexer.lpr
@@ -13,38 +13,49 @@ var
   LIndexer : TYTIndexer;
   LJSON, LError : String;
 begin
-  LIndexer := TYTIndexer.Create;
+  LIndexer := TYTIndexer.Create(nil);
+  try
+    //load settings when we have them
+    if FileExists('appsettings.json') then
+      with TStringStream.Create do
+      begin
+        try
+          LoadFromFile('appsettings.json');
+          LJSON := DataString;
 
-  //load settings when we have them
-  if FileExists('appsettings.json') then
-    with TStringStream.Create do
+          if not (EZDeserialize<TYTIndexer>(LJSON, LIndexer, LError)) then
+            WriteLn('unable to load appsettings.json [' + LError + ']');
+        finally
+          Free;
+        end;
+      end;
+
+    //start indexing videos
+    LIndexer.Run;
+
+    if LIndexer.Running then
+      WriteLn('indexing videos, press any key to stop...')
+    else
+      WriteLn(LIndexer.Error);
+
+    //wait for user
+    ReadLn;
+
+    //once finished save to the settings file
+    if not (EZSerialize<TYTIndexer>(LIndexer, LJSON, LError)) then
+      WriteLn('unable to serialize indexer [' + LError + ']');
+
+    with TStringStream.Create(LJSON) do
     begin
       try
-        LoadFromFile('appsettings.json');
-        LJSON := DataString;
-
-        if not (EZDeserialize<TYTIndexer>(LJSON, LIndexer, LError)) then
-          WriteLn('unable to load appsettings.json [' + LError + ']');
+        SaveToFile('appsettings.json');
       finally
         Free;
       end;
     end;
-
-  //todo... index videos
-  LIndexer.Run;
-
-  //once finished save to the settings file
-  if not (EZSerialize<TYTIndexer>(LIndexer, LJSON, LError)) then
-    WriteLn('unable to serialize indexer [' + LError + ']');
-
-  with TStringStream.Create(LJSON) do
-  begin
-    try
-      SaveToFile('appsettings.json');
-    finally
-      Free;
-    end;
+    ReadLn;
+  finally
+    LIndexer.Free;
   end;
-  ReadLn;
 end.
 
